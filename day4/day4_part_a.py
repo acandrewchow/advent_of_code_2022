f587adb8d78c074fdda9d9f39d435cd7346851ac
Andrew Chow <40132839+acandrewchow@users.noreply.github.com>
2022-12-04 01:42:46 -0500

    Add files via upload

---
+++ day4/day4_part_a.py
@@ -0,0 +1,23 @@
+with open ("data.txt") as f:
+    data = [i for i in f.read().strip().split("\n")]
+
+count = 0
+
+for x in data:
+    #  50-50, 50-87
+    # a1 = 50, a2 = 50
+    # b1 = 50, b2 = 87
+    
+    a, b = x.split(",")
+    a1, a2 = a.split("-")
+    b1, b2 = b.split("-")
+
+    if int(a1) <= int(b1) and int(a2) >= int(b2):
+        count += 1
+    elif int(b1) <= int(a1) and int(b2) >= int(a2):
+        count += 1
+
+print("Part 1: ", count)
+  
+
+ 
