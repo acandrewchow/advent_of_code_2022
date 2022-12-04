1e4f9fed2ebc117b80181d0b2cff8e6518b8c4ff
Andrew Chow <40132839+acandrewchow@users.noreply.github.com>
2022-12-04 02:03:15 -0500

    Update day4_part_a.py

--- day4/day4_part_a.py
+++ day4/day4_part_a.py
@@ -14,7 +14,7 @@ for x in data:
 
     if int(a1) <= int(b1) and int(a2) >= int(b2):
         count += 1
-    elif int(b1) <= int(a1) and int(b2) >= int(a2):
+    elif int(a1) >= int(b1) and int(a2) <= int(b2):
         count += 1
 
 print("Part 1: ", count)
