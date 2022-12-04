e57049eb8ea7053643b8b3d31dd1668c4fb6f267
Andrew Chow <40132839+acandrewchow@users.noreply.github.com>
2022-12-04 14:30:32 -0500

    adding visual comments

--- day4/day4_part_a.py
+++ day4/day4_part_a.py
@@ -8,12 +8,22 @@ for x in data:
     # a1 = 50, a2 = 50
     # b1 = 50, b2 = 87
     
+    # Case 1
+    # b1 ---------------- b2
+    #   a1 ------------ a2
+
+    # Case 2
+    #    b1 ------ b2
+    # a1 ------------- a2
+    
     a, b = x.split(",")
     a1, a2 = a.split("-")
     b1, b2 = b.split("-")
-
+    
+    # case 2
     if int(a1) <= int(b1) and int(a2) >= int(b2):
         count += 1
+    # case 1
     elif int(a1) >= int(b1) and int(a2) <= int(b2):
         count += 1
 
